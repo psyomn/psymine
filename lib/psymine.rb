e357320b3dc57fe230d774a08869a0cdcc10bd4d
psyomn <lethaljellybean@gmail.com>
2013-06-24 21:37:49 -0400

    Add the possibility of fetching Project json.

--- lib/psymine.rb
+++ lib/psymine.rb
@@ -1,1 +1,27 @@
 require 'psymine/version'
+require 'psymine/technical/retriever'
+
+# @author  Simon Symeonidis
+# @license GPL v3.0
+# @date    Mon Jun 24 21:56:41 EDT 2013
+module Psymine
+  include Psymine::Technical
+
+  # Fetch all the issues
+  def self.fetch_issues_by_api_key(uri, api_key)
+    ret = Retriever.new({
+      :api_key => api_key, 
+      :uri => uri, 
+      :get => :issues})
+    ret.fetch!
+  end
+
+  # Fetch all the projects
+  def self.fetch_projects_by_api_key(uri, api_key)
+    ret = Retriever.new({
+        :api_key => api_key, 
+        :uri => uri,
+        :get => :projects})
+    ret.fetch!
+  end
+end # module Psymine
--- lib/psymine/presentation/facade.rb
+++ lib/psymine/presentation/facade.rb
@@ -1,21 +1,12 @@
 require 'psymine'
-require 'psymine/technical/retriever'
 module Psymine::Presentation
 # @author  Simon Symeonidis
 # @license GPL v3.0
 # @date Mon Jun 24 19:31:18 EDT 2013
 # Facade controller for other applications to (easy) interface with this gem 
 # library.
-class Facade
-
-  def self.fetch_issues_by_api_key(redmine_uri,api_key)
-    include Psymine::Technical
-    ret = Retriever.new({:api_key => api_key, 
-                         :uri => redmine_uri, :get => :issues})
-    ret.fetch!
-  end
-
+module Facade
 private
 
-end # class TimeEntry
+end # module Facade
 end # module Psymine
